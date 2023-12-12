3b7dc9f5114eef28479ea84543e87360f59c6300
lapin <delcoignevincent@mailbox.org>
2023-12-12 21:58:36 +0100

    Day 3 wip

--- 2023/Day3.fsx
+++ 2023/Day3.fsx
@@ -16,23 +16,20 @@ let example =
 ...$.*....
 .664.598.."""
         .Split("\n")
-    |> Array.map (_.Trim())
+    |> Array.map (fun s -> s.Trim())
     |> List.ofSeq
 
 type State =
     | Symbol of string
-    | Digit of int
+    | Digit of string
     | Empty
 
 type Position = int * int
 
-type Cell =
-    | Position of Position
-    | State of State
-    
+
 let parse =
     function
-    | c when c |> Char.IsDigit -> Digit(c |> string |> int)
+    | c when c |> Char.IsDigit -> Digit(c |> string)
     | c when c = '.' -> Empty
     | c -> Symbol(c |> string)
 
@@ -40,8 +37,48 @@ let lines =
     [ for row, line in (example |> Seq.indexed) do
           [ for col, char in line |> Seq.indexed -> ((col, row), char |> parse) ] ]
 
-let line = [((0, 0), Digit 4); ((1, 0), Digit 6); ((2, 0), Digit 7); ((3, 0), Empty);
-    ((4, 0), Empty); ((5, 0), Digit 1); ((6, 0), Digit 1); ((7, 0), Digit 4);
-    ((8, 0), Empty); ((9, 0), Empty)]
+// let line =
+//     [ ((0, 0), Digit "4")
+//       ((1, 0), Digit "6")
+//       ((2, 0), Digit "7")
+//       ((3, 0), Empty)
+//       ((4, 0), Empty)
+//       ((5, 0), Digit "1")
+//       ((6, 0), Digit "1")
+//       ((7, 0), Digit "4")
+//       ((8, 0), Empty)
+//       ((9, 0), Empty) ]
 
-line |> List.takeWhile (fun (_, state) -> match state with | Digit _ -> true | _ -> false)
+let digit(Digit d) = d
+
+type Bundled =
+    | Empty
+    | Number of int
+    | Symbol of string
+
+let rec bundle(line: (Position * State) list) =
+    match line with
+    | [] -> []
+    | (_, Digit _) :: t ->
+        let bundled =
+            line
+            |> List.takeWhile (fun (_, state) ->
+                match state with
+                | Digit _ -> true
+                | _ -> false)
+
+        let positions = bundled |> List.map fst
+        let number = bundled |> List.map (snd >> digit) |> String.concat "" |> int |> Number
+
+        let rest = line |> List.skip (bundled |> List.length)
+        (positions, number) :: bundle rest
+    | (pos, otherwise) :: t ->
+        let entry =
+            match otherwise with
+            | State.Symbol s -> Bundled.Symbol s
+            | State.Empty -> Bundled.Empty
+
+        let c = [ pos ], entry
+        c :: (bundle t)
+
+let c = lines |> List.collect bundle 
