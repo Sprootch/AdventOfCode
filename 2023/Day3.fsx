016e2589517a4962d4ed6f74b6ee6b21d3509891
Vincent DELCOIGNE <vdelcoigne@actiris.be>
2023-12-13 15:45:08 +0100

    Solve day 3

--- 2023/Day3.fsx
+++ 2023/Day3.fsx
@@ -1,8 +1,7 @@
 module Day3
 
 open System
-
-let solve input = 0
+open System.IO
 
 let example =
     """467..114..
@@ -32,9 +31,6 @@ let parse =
     | c when c = '.' -> Empty
     | c -> Symbol(c |> string)
 
-let lines =
-    [ for row, line in (example |> Seq.indexed) do
-          [ for col, char in line |> Seq.indexed -> ((col, row), char |> parse) ] ]
 
 let digit(Digit d) = d
 
@@ -68,7 +64,13 @@ let rec bundle(line: (Position * State) list) =
         let c = [ pos ], entry
         c :: (bundle t)
 
-let bundled = lines |> List.collect bundle
+let bundled =
+    let lines input =
+        [ for row, line in (input |> Seq.indexed) do
+              [ for col, char in line |> Seq.indexed -> ((col, row), char |> parse) ] ]
+
+    let example = File.ReadAllLines @"C:\Projects\Perso\AdventOfCode\2023\Input\day3.txt"
+    lines example |> List.collect bundle
 
 let symbols =
     bundled
@@ -89,6 +91,7 @@ let isTouching (position: Position) ((numberPos, _): Position list * Bundled) =
     let x, y = position
 
     let c = numberPos |> Set.ofList
+
     let neighbours =
         [ x - 1, y - 1
           x, y - 1
@@ -97,12 +100,20 @@ let isTouching (position: Position) ((numberPos, _): Position list * Bundled) =
           x + 1, y
           x - 1, y + 1
           x, y + 1
-          x + 1, y + 1 ] |> Set.ofList
+          x + 1, y + 1 ]
+        |> Set.ofList
 
     let x = Set.intersect c neighbours |> Set.count
     x <> 0
-    
-let hasSymbol  (symbolPos : Position list) (number: Position list * Bundled) =
+
+let hasSymbol (symbolPos: Position list) (number: Position list * Bundled) =
     symbolPos |> List.exists (fun pos -> isTouching pos number)
-    
-let parts = numbers |> List.filter (hasSymbol symbols) |> List.map snd |> List.map (fun n -> match n with | Number n -> n) |> List.sum
+
+let parts =
+    numbers
+    |> List.filter (hasSymbol symbols)
+    |> List.map snd
+    |> List.map (fun n ->
+        match n with
+        | Number n -> n)
+    |> List.sum
