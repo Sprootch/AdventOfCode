5da8749066ad4965f03ebe38bcd83e18bfb8a9e3
lapin <delcoignevincent@mailbox.org>
2023-12-23 19:48:24 +0100

    Day7: refacto

--- 2023/Day7.fsx
+++ 2023/Day7.fsx
@@ -19,8 +19,7 @@ type HandType =
 type Hand =
     { Bid: int
       Cards: Card list
-      Type: HandType
-      Strength: int }
+      Type: HandType }
 
 let makeHand =
     function
@@ -31,60 +30,60 @@ let makeHand =
     | 'T' -> Number 10
     | n -> n |> string |> int |> Number
 
-let getType cards =
-    // todo : take snd only?
-    match cards |> List.countBy id |> List.sortByDescending snd with
-    | [ (_, 5) ] -> FiveOfAKind
-    | [ (_, 4); _ ] -> FourOfAKind
-    | [ (_, 3); (_, 2) ] -> FullHouse
-    | [ (_, 3); _; _ ] -> ThreeOfAKind
-    | [ (_, 2); (_, 2); _ ] -> TwoPair
-    | [ (_, 2); _; _; _ ] -> OnePair
+let getType(cards: Card list) =
+    match cards |> List.countBy id |> List.map snd |> List.sortDescending with
+    | [ 5 ] -> FiveOfAKind
+    | [ 4; _ ] -> FourOfAKind
+    | [ 3; 2 ] -> FullHouse
+    | [ 3; _; _ ] -> ThreeOfAKind
+    | [ 2; 2; _ ] -> TwoPair
+    | [ 2; _; _; _ ] -> OnePair
     | _ -> HighCard
 
-let getHandStrength =
-    function
-    | FiveOfAKind -> 6
-    | FourOfAKind -> 5
-    | FullHouse -> 4
-    | ThreeOfAKind -> 3
-    | TwoPair -> 2
-    | OnePair -> 1
-    | HighCard -> 0
-
-let getCardStrength =
-    function
-    | As -> 14
-    | King -> 13
-    | Queen -> 12
-    | Jack -> 11
-    | Number n -> n
-    
 let createHand(line: string) =
     let splitted = line.Split(' ')
     let cards = splitted[0] |> Seq.map makeHand |> Seq.toList
     let bid = splitted[1] |> int
     let type' = cards |> getType
-    let strength = type' |> getHandStrength
 
     { Bid = bid
       Cards = cards
-      Strength = strength
       Type = type' }
 
 let compareCard(card1, card2) =
-    compare (card2 |> getCardStrength) (card1 |> getCardStrength)
+    let cardStrength =
+        function
+        | As -> 14
+        | King -> 13
+        | Queen -> 12
+        | Jack -> 11
+        | Number n -> n
+
+    compare (card2 |> cardStrength) (card1 |> cardStrength)
 
 let comparer hand1 hand2 =
-    if (hand1.Strength > hand2.Strength) then
-        1
-    else if (hand2.Strength > hand1.Strength) then
-        -1
-    else
+    let handStrength =
+        function
+        | FiveOfAKind -> 6
+        | FourOfAKind -> 5
+        | FullHouse -> 4
+        | ThreeOfAKind -> 3
+        | TwoPair -> 2
+        | OnePair -> 1
+        | HighCard -> 0
+
+    let strength1 = hand1.Type |> handStrength
+    let strength2 = hand2.Type |> handStrength
+
+    if (strength1 = strength2) then
         hand1.Cards
         |> List.zip hand2.Cards
         |> List.map compareCard
         |> List.find ((<>) 0)
+    else if (strength1 > strength2) then
+        1
+    else
+        -1
 
 let example =
     """32T3K 765
@@ -105,5 +104,5 @@ let solve input =
 
 // solve example
 
-let input = File.ReadAllText(Path.Combine("2023", "Input", "day7.txt")).Split("\n")
+let input = File.ReadAllLines(Path.Combine("2023", "Input", "day7.txt"))
 solve input
