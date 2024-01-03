85d22bb42f1b8170ec7949f03dfe1e79b497ebc4
Vincent DELCOIGNE <vdelcoigne@actiris.be>
2024-01-03 12:08:02 +0100

    Day 8 : solved ? very slow

--- 2023/Day8.fsx
+++ 2023/Day8.fsx
@@ -1,3 +1,5 @@
+open System.IO
+
 type Node =
     { Value: string
       Left: string
@@ -15,64 +17,56 @@ let makeInstructions(line: string) : Direction list =
         | _ -> failwith "invalid direction")
     |> Seq.toList
 
+let makeNodes(lines: string array) =
+    lines
+    |> Array.map (fun line ->
+        let value = line[0..2]
+        let left = line[7..9]
+        let right = line[12..14]
+
+        { Value = value
+          Right = right
+          Left = left })
+
 let navigate node nodes direction =
     let value =
         match direction with
         | Left -> node.Left
         | Right -> node.Right
 
-    nodes |> List.find (fun n -> n.Value = value)
+    nodes |> Array.find (fun n -> n.Value = value)
 
-let rec traverse (node: Node) (nodes: Node list) (directions: Direction seq) (count: int) =
+let rec traverse (node: Node) (nodes: Node array) (directions: Direction seq) (count: int) =
     if node.Value = "ZZZ" then
         count
     else
         let direction = directions |> Seq.head
-        // printfn $"%s{node.Value} -> %A{direction}"
+        printfn $"%s{node.Value} -> %A{direction}"
         let nextNode = navigate node nodes direction
-        // printfn $"Next node is %s{nextNode.Value}"
+        printfn $"Next node is %s{nextNode.Value}"
         traverse nextNode nodes (directions |> Seq.tail) (count + 1)
 
 let example =
-    """RL
+    """LLR
 
-AAA = (BBB, CCC)
-BBB = (DDD, EEE)
-CCC = (ZZZ, GGG)
-DDD = (DDD, DDD)
-EEE = (EEE, EEE)
-GGG = (GGG, GGG)
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)"""
 
-let lines = example.Split("\n")
-lines[0] |> makeInstructions
+let lines = File.ReadAllLines(Path.Combine("2023", "Input", "day8.txt"))
 
-// lines[2..] |> makeNodes
+// let lines = input.Split("\n")
+let directions = lines[0] |> makeInstructions
 
-let zzz =
-    { Value = "ZZZ"
-      Left = "ZZZ"
-      Right = "ZZZ" }
-
-let aaa =
-    { Value = "AAA"
-      Right = "BBB"
-      Left = "BBB" }
-
-let bbb =
-    { Value = "BBB"
-      Left = "AAA"
-      Right = "ZZZ" }
-
-let nodes = [ aaa; bbb; zzz ]
+let nodes = lines[2..] |> makeNodes
 
 let generateDirections index =
-    let directions = [ Left; Left; Right ]
+    // let directions = [ Left; Left; Right ]
     directions[index % directions.Length]
 
 let generator = Seq.initInfinite generateDirections
 
 // generator |> Seq.take 20 |> Seq.toList |> List.iter (printfn "%A")
 
-let firstNode = nodes |> List.head
+let firstNode = nodes |> Array.head
 traverse firstNode nodes generator 0
