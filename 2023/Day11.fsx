8cb06c40e0c6a3b808e3e61b5bdecb3d7ba5da03
Vincent DELCOIGNE <vdelcoigne@actiris.be>
2024-01-03 16:22:40 +0100

    Day 11 : find empty rows / cols

--- 2023/Day11.fsx
+++ 2023/Day11.fsx
@@ -25,7 +25,24 @@ let parse =
 
 let lines input =
     [ for row, line in (input |> Seq.indexed) do
-          [ for col, char in line |> Seq.indexed -> ((col, row), char |> parse) ] ]
+          [ for col, char in line |> Seq.indexed -> ((row, col), char |> parse) ] ]
+    |> List.collect id
 
-let x = lines example
-x |> List.collect id
+let solve =
+    let space = lines example
+
+    for row in 0..9 do
+        let isEmpty =
+            space
+            |> List.filter (fun ((x, _), _) -> x = row)
+            |> List.forall (fun (_, space) -> space = Empty)
+
+        printfn $"""Row %d{row} %A{if isEmpty then "empty" else "not empty"}"""
+
+    for col in 0..9 do
+        let isEmpty =
+            space
+            |> List.filter (fun ((_, y), _) -> y = col)
+            |> List.forall (fun (_, space) -> space = Empty)
+
+        printfn $"""Col %d{col} %A{if isEmpty then "empty" else "not empty"}"""
