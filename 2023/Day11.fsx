187f11e0edbee33cbf41cef1e1756663d1531280
lapin <delcoignevincent@mailbox.org>
2024-01-05 10:56:51 +0100

    Day 11 : Array2D

--- 2023/Day11.fsx
+++ 2023/Day11.fsx
@@ -28,21 +28,30 @@ let lines input =
           [ for col, char in line |> Seq.indexed -> ((row, col), char |> parse) ] ]
     |> List.collect id
 
-let solve =
-    let space = lines example
+let lines2 input array =
+    for row, line in (input |> Seq.indexed) do
+          for col, char in line |> Seq.indexed do
+                Array2D.set array row col (char |> parse)
+    
+let universe = Array2D.zeroCreate 10 10
+lines2 example universe
 
-    for row in 0..9 do
-        let isEmpty =
-            space
-            |> List.filter (fun ((x, _), _) -> x = row)
-            |> List.forall (fun (_, space) -> space = Empty)
+universe[7,*] |> Array.forall (fun s -> s = Empty)
+// let solve =
+    // let space = lines example
 
-        printfn $"""Row %d{row} %A{if isEmpty then "empty" else "not empty"}"""
-
-    for col in 0..9 do
-        let isEmpty =
-            space
-            |> List.filter (fun ((_, y), _) -> y = col)
-            |> List.forall (fun (_, space) -> space = Empty)
-
-        printfn $"""Col %d{col} %A{if isEmpty then "empty" else "not empty"}"""
+    // for row in 0..9 do
+    //     let isEmpty =
+    //         space
+    //         |> List.filter (fun ((x, _), _) -> x = row)
+    //         |> List.forall (fun (_, space) -> space = Empty)
+    //
+    //     printfn $"""Row %d{row} %A{if isEmpty then "empty" else "not empty"}"""
+    //
+    // for col in 0..9 do
+    //     let isEmpty =
+    //         space
+    //         |> List.filter (fun ((_, y), _) -> y = col)
+    //         |> List.forall (fun (_, space) -> space = Empty)
+    //
+    //     printfn $"""Col %d{col} %A{if isEmpty then "empty" else "not empty"}"""
