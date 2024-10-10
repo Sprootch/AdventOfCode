2166cfb9801efb88a4b8bd328bc1af399ea9cdce
Vincent DELCOIGNE <vdelcoigne@actiris.be>
2024-10-10 16:14:30 +0200

    Day 8 WIP

--- 2022/Day8.fsx
+++ 2022/Day8.fsx
@@ -1,6 +1,14 @@
 open System.IO
 
 let txt = File.ReadLines(Path.Combine("2022/Input", "day8.txt"))
-let map = [ for line in txt -> [ for tree in line -> tree |> string |> int ] ]
+// let trees = [| for line in txt -> [| for tree in line -> tree |> string |> int |] |]
+let trees = array2D [| for line in txt -> [| for tree in line -> tree |> string |> int |] |]
+printfn "%A" trees
 
-map[1][4]
+trees[1,*]
+
+// [ for row in trees -> [ for col in row -> printfn "%A"col ] ]
+// map |> List.iteri (fun i row -> printfn $"row %d{i} is %A{row}")
+
+// let e = map |> List.length
+// [1..e-2] |> List.iter (fun i -> printfn $"%A{map[i]}")
