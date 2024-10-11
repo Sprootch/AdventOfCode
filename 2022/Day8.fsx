a4ea34efac63f5a750cad482fd52f2b4c5d3015a
Vincent DELCOIGNE <vdelcoigne@actiris.be>
2024-10-11 16:03:41 +0200

    Day8

--- 2022/Day8.fsx
+++ 2022/Day8.fsx
@@ -2,10 +2,19 @@ open System.IO
 
 let txt = File.ReadLines(Path.Combine("2022/Input", "day8.txt"))
 // let trees = [| for line in txt -> [| for tree in line -> tree |> string |> int |] |]
-let trees = array2D [| for line in txt -> [| for tree in line -> tree |> string |> int |] |]
+let trees =
+    array2D [ for line in txt -> [ for tree in line -> tree |> string |> int ] ]
+
 printfn "%A" trees
 
-trees[1,*]
+let isVisible row col treeHeight =
+    let allExceptMe = trees[row,*]|> List.filter (fun )//TODO
+
+    allExceptMe |> List.forall (fun height -> height < treeHeight)
+
+trees |> Array2D.mapi (fun row col value -> $"({row}, {col} = {value}")
+// columns
+let x = [ for i in 0..4 -> trees[i, 0] ]
 
 // [ for row in trees -> [ for col in row -> printfn "%A"col ] ]
 // map |> List.iteri (fun i row -> printfn $"row %d{i} is %A{row}")
