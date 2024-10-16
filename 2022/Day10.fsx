a6c0b3978569339b36f781a2ee03091db4e204fe
Vincent DELCOIGNE <vdelcoigne@actiris.be>
2024-10-16 11:10:25 +0200

    Day 10

--- 2022/Day10.fsx
+++ 2022/Day10.fsx
@@ -5,6 +5,8 @@ let lines = File.ReadLines(Path.Combine("2022/Input", "day10.txt"))
 type Cpu = { CycleCount: int; Register: int }
 
 let scanner (cpu: Cpu) (line: string) =
+    // printfn $"{cpu.CycleCount} : {cpu.Register}"
+    // printfn $"{line}"
     match line.Split(' ') with
     | [| "addx"; count |] ->
         let value = count |> int
@@ -12,16 +14,25 @@ let scanner (cpu: Cpu) (line: string) =
         { cpu with
             CycleCount = cpu.CycleCount + 2
             Register = cpu.Register + value }
-    | _ ->
+    | [|"noop"|] ->
         { cpu with
             CycleCount = cpu.CycleCount + 1 }
+    | _ -> cpu
+
+let rec find cycleCount instructions =
+    match instructions |> Seq.tryFind (fun c -> c.CycleCount = cycleCount) with
+    | Some cpu -> cpu
+    | None -> find (cycleCount - 1) instructions
+
+let signalStrength cycleCount instructions =
+    let cpu = instructions |> find (cycleCount-1)
+    cpu.Register * cycleCount
 
 let cpu = { CycleCount = 0; Register = 1 }
 let instructions = lines |> Seq.scan scanner cpu
-instructions |> Seq.tryFind (fun c -> c.CycleCount = 20)
-instructions |> Seq.tryFind (fun c -> c.CycleCount = 60)
-instructions |> Seq.tryFind (fun c -> c.CycleCount = 100)
-instructions |> Seq.tryFind (fun c -> c.CycleCount = 140)
-instructions |> Seq.tryFind (fun c -> c.CycleCount = 180)
-instructions |> Seq.tryFind (fun c -> c.CycleCount = 220)
-// lines |> Seq.scan (fun l -> ) list.Empty
+
+[ 20; 60; 100; 140; 180; 220 ]
+|> List.map (fun cycle -> instructions |> signalStrength cycle)
+|> List.sum
+
+instructions |> find 220
