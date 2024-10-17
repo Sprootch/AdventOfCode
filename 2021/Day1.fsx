03bddd6c3e1582fd0bfc791794f2865ed18ee7c7
Vincent DELCOIGNE <vdelcoigne@actiris.be>
2024-10-17 10:31:21 +0200

    Day 1 part 2

--- 2021/Day1.fsx
+++ 2021/Day1.fsx
@@ -3,5 +3,21 @@ open System.IO
 File.ReadLines(Path.Combine("2021/Input", "day1.txt"))
 |> Seq.map int
 |> Seq.pairwise
-|> Seq.map (fun (current, next) -> next > current)
-|> Seq.countBy id
+|> Seq.map (fun (current, next) -> if next > current then 1 else 0)
+|> Seq.sum
+
+module Array =
+    let pairwise3 (array: array<'a>) =
+        seq {
+            for i in 0 .. array.Length - 3 do
+                yield (array[i], array[i + 1], array[i + 2])
+        } |> Seq.toArray
+
+File.ReadLines(Path.Combine("2021/Input", "day1_2.txt"))
+|> Seq.map int
+|> Seq.toArray
+|> Array.pairwise3
+|> Array.map (fun (a, b, c) -> a + b + c)
+|> Array.pairwise
+|> Array.map (fun (current, next) -> if next > current then 1 else 0)
+|> Array.sum
