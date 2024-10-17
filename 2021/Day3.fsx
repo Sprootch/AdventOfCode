917bbbb77924184610f4696a3753c0b4a02318e5
lapin <delcoignevincent@mailbox.org>
2024-10-17 20:59:03 +0200

    Refacto

--- 2021/Day3.fsx
+++ 2021/Day3.fsx
@@ -8,6 +8,13 @@ let getColumn idx array =
 
 let bytes = array2D [ for line in lines -> [ for bit in line -> bit ] ]
 
+let compute2 (pick: ('t -> 'u) -> 'x seq -> 'x) =
+    let size = (bytes |> Array2D.length2) - 1
+
+    [ for i in 0..size -> bytes |> getColumn i |> Seq.countBy id |> (pick snd) |> fst ]
+    |> System.String.Concat
+    |> (fun binary -> System.Convert.ToInt32(binary, 2))
+
 let compute (pick: (char * int) seq -> char * 'a) =
     let size = (bytes |> Array2D.length2) - 1
 
@@ -15,6 +22,6 @@ let compute (pick: (char * int) seq -> char * 'a) =
     |> System.String.Concat
     |> (fun binary -> System.Convert.ToInt32(binary, 2))
 
-let gamma = compute (Seq.maxBy snd)
-let epsilon = compute (Seq.minBy snd)
+let gamma = compute2 Seq.maxBy
+let epsilon = compute2 Seq.minBy
 gamma * epsilon
