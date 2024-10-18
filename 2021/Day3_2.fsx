386eb23b6a9e89f2b145d30c14e2bc2f828699b7
Vincent DELCOIGNE <vdelcoigne@actiris.be>
2024-10-18 12:05:30 +0200

    Refacto

--- 2021/Day3_2.fsx
+++ 2021/Day3_2.fsx
@@ -2,40 +2,36 @@ open System.IO
 
 let lines = File.ReadLines(Path.Combine("2021/Input", "day3_2.txt"))
 
-let getColumn idx (list: char list list) =
-    seq { for item in list -> item[idx] } |> Seq.toList
+let getColumn idx (list: char list list) = seq { for item in list -> item[idx] }
 
 let toDecimal (chars: char list) =
     chars
     |> System.String.Concat
     |> (fun binary -> System.Convert.ToInt32(binary, 2))
 
-// todo merge
-let rec computeOxygen index bytes =
+let compute selector bytes =
     let get char seq =
-        seq |> List.find (fun c -> fst c = char) |> snd
+        seq |> Seq.find (fun c -> fst c = char) |> snd
 
-    let countBy = bytes |> getColumn index |> List.countBy id
-    let zeros = countBy |> get '0'
-    let ones = countBy |> get '1'
+    let rec innerCompute index bytes =
+        let countBy = bytes |> getColumn index |> Seq.countBy id
 
-    let winner = if (zeros > ones) then '0' else '1'
-    let n = bytes |> List.filter (fun c -> c[index] = winner)
-    if (n.Length = 1) then n else n |> computeOxygen (index + 1)
+        let winner = selector (countBy |> get '0') (countBy |> get '1')
+        let filtered = bytes |> List.filter (fun c -> c[index] = winner)
 
-let rec computeNitro index bytes =
-    let get char seq =
-        seq |> List.find (fun c -> fst c = char) |> snd
+        if (filtered.Length = 1) then
+            filtered
+        else
+            filtered |> innerCompute (index + 1)
 
-    let countBy = bytes |> getColumn index |> List.countBy id
-    let zeros = countBy |> get '0'
-    let ones = countBy |> get '1'
+    bytes |> innerCompute 0
 
-    let winner = if (ones < zeros) then '1' else '0'
-    let n = bytes |> List.filter (fun c -> c[index] = winner)
-    if (n.Length = 1) then n else n |> computeNitro (index + 1)
+let computeOxygen =
+    compute (fun zeroes ones -> if (zeroes > ones) then '0' else '1')
+
+let computeNitro = compute (fun zeroes ones -> if (ones < zeroes) then '1' else '0')
 
 let bytes = [ for line in lines -> [ for bit in line -> bit ] ]
-let oxygen = bytes |> computeOxygen 0 |> List.head |> toDecimal
-let nitroxyde = bytes |> computeNitro 0 |> List.head |> toDecimal
+let oxygen = bytes |> computeOxygen |> List.head |> toDecimal
+let nitroxyde = bytes |> computeNitro |> List.head |> toDecimal
 oxygen * nitroxyde
