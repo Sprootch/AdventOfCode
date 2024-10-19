99ea3270d4442b269696d33c291ad3f8ce0ac97a
lapin <delcoignevincent@mailbox.org>
2024-10-19 14:34:08 +0200

    mkdraw

--- 2021/Day4.fsx
+++ 2021/Day4.fsx
@@ -1,7 +1,7 @@
 open System
 open System.IO
 
-type BingoCard = int array2d
+type BingoCard = (int * bool) array2d
 
 let lines = File.ReadLines(Path.Combine("2021/Input", "day4.txt"))
 
@@ -15,9 +15,36 @@ let folder state line =
         let bingoCard = state |> List.head
         state |> List.updateAt 0 (line :: bingoCard)
 
+let mkDraw value (bingoCard: BingoCard) : BingoCard =
+    bingoCard
+    |> Array2D.map (fun tuple -> if fst tuple = value then (value, true) else tuple)
+
 let createBingoCard (card: string list) : BingoCard =
     array2D
         [ for line in (card |> List.rev) ->
-              [ for str in line.Split(' ', StringSplitOptions.RemoveEmptyEntries) -> str |> int ] ]
+              [ for str in line.Split(' ', StringSplitOptions.RemoveEmptyEntries) -> (str |> int, false) ] ]
 
-lines |> Seq.skip 1 |> Seq.fold folder List.empty |> List.map createBingoCard
+let hasWon (bingoCard: BingoCard) : bool =
+    // let x = bingoCard[0, 0]
+    let x =
+        [| for i in 0..4 -> bingoCard[i, 0] |]
+        |> Array.forall (fun spot -> snd spot = true)
+
+    false
+
+let bingoCards =
+    lines |> Seq.skip 1 |> Seq.fold folder List.empty |> List.map createBingoCard
+
+let first = bingoCards |> List.head
+let nbRows = 5 // todo
+[| for i in 0 .. (nbRows - 1) -> first[i, *] |> Array.forall (fun x -> snd x = true) |] |> Array.fold (fun a b -> a || b) false
+[|false; false; true; false; false|]|> Array.fold (fun a b -> a || b) false
+first[0, *] // first row
+first[*, 0] // first column
+
+let mkDraws bingoCards value =
+    let bingoCards = bingoCards |> List.map (mkDraw value)
+    if (bingoCards |> List.exists hasWon) then [] else []
+
+
+draw |> Array.fold mkDraws bingoCards
