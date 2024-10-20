56a4cea925c3f5e67f61b2290d8ad0e8728fa0e2
lapin <delcoignevincent@mailbox.org>
2024-10-20 17:52:25 +0200

    2021 day 4 solved

--- 2021/Day4.fsx
+++ 2021/Day4.fsx
@@ -7,44 +7,57 @@ let lines = File.ReadLines(Path.Combine("2021/Input", "day4.txt"))
 
 let draw = lines |> Seq.head |> (_.Split(',')) |> Array.map int
 
-let folder state line =
-    if (String.IsNullOrWhiteSpace line) then
-        let newBingoCard = list.Empty
-        newBingoCard :: state
-    else
-        let bingoCard = state |> List.head
-        state |> List.updateAt 0 (line :: bingoCard)
-
 let mkDraw value (bingoCard: BingoCard) : BingoCard =
     bingoCard
     |> Array2D.map (fun tuple -> if fst tuple = value then (value, true) else tuple)
 
-let createBingoCard (card: string list) : BingoCard =
-    array2D
-        [ for line in (card |> List.rev) ->
-              [ for str in line.Split(' ', StringSplitOptions.RemoveEmptyEntries) -> (str |> int, false) ] ]
+let hasWon bingoCard =
+    let rowWon =
+        let nbRows = bingoCard |> Array2D.length2
 
-let hasWon (bingoCard: BingoCard) : bool =
-    // let x = bingoCard[0, 0]
-    let x =
-        [| for i in 0..4 -> bingoCard[i, 0] |]
-        |> Array.forall (fun spot -> snd spot = true)
+        [| for i in 0 .. (nbRows - 1) -> bingoCard[i, *] |> Array.forall (fun x -> snd x = true) |]
+        |> Array.fold (fun a b -> a || b) false
 
-    false
+    let colWon =
+        let nbCols = bingoCard |> Array2D.length1
+
+        [| for i in 0 .. (nbCols - 1) -> bingoCard[*, i] |> Array.forall (fun x -> snd x = true) |]
+        |> Array.fold (fun a b -> a || b) false
+
+    rowWon || colWon
 
 let bingoCards =
+    let folder state line =
+        if (String.IsNullOrWhiteSpace line) then
+            let newBingoCard = list.Empty
+            newBingoCard :: state
+        else
+            let bingoCard = state |> List.head
+            state |> List.updateAt 0 (line :: bingoCard)
+
+    let createBingoCard (card: string list) : BingoCard =
+        array2D
+            [ for line in (card |> List.rev) ->
+                  [ for str in line.Split(' ', StringSplitOptions.RemoveEmptyEntries) -> (str |> int, false) ] ]
+
     lines |> Seq.skip 1 |> Seq.fold folder List.empty |> List.map createBingoCard
 
-let first = bingoCards |> List.head
-let nbRows = 5 // todo
-[| for i in 0 .. (nbRows - 1) -> first[i, *] |> Array.forall (fun x -> snd x = true) |] |> Array.fold (fun a b -> a || b) false
-[|false; false; true; false; false|]|> Array.fold (fun a b -> a || b) false
-first[0, *] // first row
-first[*, 0] // first column
+let rec runBingo draws bingoCards =
+    let numberDrawn = draws |> Array.head
+    let bingoCards = bingoCards |> List.map (mkDraw numberDrawn)
 
-let mkDraws bingoCards value =
-    let bingoCards = bingoCards |> List.map (mkDraw value)
-    if (bingoCards |> List.exists hasWon) then [] else []
+    bingoCards
+    |> List.tryFind (fun card -> card |> hasWon)
+    |> function
+        | Some card -> (numberDrawn, card)
+        | None -> runBingo (draws[1..]) bingoCards
 
+let lastNumber, winningCard = runBingo draw bingoCards
 
-draw |> Array.fold mkDraws bingoCards
+let sumUndrawn =
+    winningCard
+    |> Seq.cast<int * bool>
+    |> Seq.filter (fun v -> snd v = false)
+    |> Seq.sumBy fst
+
+lastNumber * sumUndrawn
