82c1f8a12026177e874e4ff2ee50aed1d775af7e
Vincent DELCOIGNE <vdelcoigne@actiris.be>
2024-10-21 16:08:30 +0200

    Day 5 part 2 WIP

--- 2021/Day5_2.fsx
+++ 2021/Day5_2.fsx
@@ -8,15 +8,16 @@ let mkTuples (array: string array) =
 
     (array[0] |> mkTuple, array[1] |> mkTuple)
 
-let isVerticalOrHorizontal ((xs, ys), (xe, ye)) = xs = xe || ys = ye
-
 let expand ((xs, ys), (xe, ye)) =
     if xs = xe then
         (if ys > ye then [ ye..ys ] |> List.rev else [ ys..ye ])
         |> List.map (fun y -> (xs, y))
-    else
+    else if ys = ye then
         (if xs > xe then [ xe..xs ] |> List.rev else [ xs..xe ])
         |> List.map (fun x -> (x, ys))
+    else
+        // TODO
+        []
 
 let apply (map: int array2d) (x, y) =
     map[y, x] <- map[y, x] + 1
@@ -25,7 +26,6 @@ let apply (map: int array2d) (x, y) =
 let vents =
     File.ReadLines(Path.Combine("2021/Input", "day5.txt"))
     |> Seq.map (fun line -> line.Split("->", StringSplitOptions.RemoveEmptyEntries) |> mkTuples)
-    |> Seq.filter isVerticalOrHorizontal
     |> Seq.map expand
     |> Seq.collect id
     |> Seq.toList
@@ -33,4 +33,14 @@ let vents =
 let maxX = vents |> List.maxBy fst |> fst
 let maxY = vents |> List.maxBy snd |> snd
 
-vents |> List.fold apply (Array2D.create (maxX + 1) (maxY + 1) 0) |> Seq.cast<int> |> Seq.toList |> Seq.countBy (fun x -> x > 1)
+vents
+|> List.fold apply (Array2D.create (maxX + 1) (maxY + 1) 0)
+|> Seq.cast<int>
+|> Seq.toList
+|> Seq.countBy (fun x -> x > 1)
+
+let (xs, ys), (xe, ye) = ((5, 5), (8, 2))
+xs - xe
+ys - ye
+[ for i in xs .. xe ->
+    (i, 0) ]
