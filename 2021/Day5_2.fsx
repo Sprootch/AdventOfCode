627ccb6cb6686418bf9382609ba5e4bddab0a5d1
lapin <delcoignevincent@mailbox.org>
2024-10-21 21:00:20 +0200

    Day 5 part 2

--- 2021/Day5_2.fsx
+++ 2021/Day5_2.fsx
@@ -8,7 +8,14 @@ let mkTuples (array: string array) =
 
     (array[0] |> mkTuple, array[1] |> mkTuple)
 
-let expand ((xs, ys), (xe, ye)) =
+let expand ((xs: int, ys: int), (xe, ye)) =
+    let expandDiagonally() =
+        let inc = if xs < xe then 1 else -1
+        let inc2 = if ys < ye then 1 else -1
+
+        let l1 = [ for i in 0 .. Math.Abs(xs - xe) -> i * inc ]
+        let l2 = [ for i in 0 .. Math.Abs(ys - ye) -> i * inc2 ]
+        List.zip l1 l2 |> List.map (fun (a, b) -> (xs + a, ys + b))
     if xs = xe then
         (if ys > ye then [ ye..ys ] |> List.rev else [ ys..ye ])
         |> List.map (fun y -> (xs, y))
@@ -16,8 +23,7 @@ let expand ((xs, ys), (xe, ye)) =
         (if xs > xe then [ xe..xs ] |> List.rev else [ xs..xe ])
         |> List.map (fun x -> (x, ys))
     else
-        // TODO
-        []
+        expandDiagonally()
 
 let apply (map: int array2d) (x, y) =
     map[y, x] <- map[y, x] + 1
@@ -34,13 +40,6 @@ let maxX = vents |> List.maxBy fst |> fst
 let maxY = vents |> List.maxBy snd |> snd
 
 vents
-|> List.fold apply (Array2D.create (maxX + 1) (maxY + 1) 0)
+|> List.fold apply (Array2D.create (maxY + 1) (maxX + 1) 0)
 |> Seq.cast<int>
-|> Seq.toList
 |> Seq.countBy (fun x -> x > 1)
-
-let (xs, ys), (xe, ye) = ((5, 5), (8, 2))
-xs - xe
-ys - ye
-[ for i in xs .. xe ->
-    (i, 0) ]
